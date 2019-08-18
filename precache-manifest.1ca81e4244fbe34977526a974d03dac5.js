9c73a6d960b5fad529b92458d32ca47457bb2c3f
tuong <nguyenhuutuong999@gmail.com>
2019-08-18 16:48:31 +0700

    Updates

---
+++ precache-manifest.1ca81e4244fbe34977526a974d03dac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7019cf8363faaa8de9282642c60b5fe7",
+    "url": "/WorkManager_Redux/index.html"
+  },
+  {
+    "revision": "821d7feff2e5637b48d7",
+    "url": "/WorkManager_Redux/static/css/main.65bdadf0.chunk.css"
+  },
+  {
+    "revision": "064596d283021204dbdc",
+    "url": "/WorkManager_Redux/static/js/2.dfd647f5.chunk.js"
+  },
+  {
+    "revision": "821d7feff2e5637b48d7",
+    "url": "/WorkManager_Redux/static/js/main.173f47c4.chunk.js"
+  },
+  {
+    "revision": "0ecf29c621fc95d9681d",
+    "url": "/WorkManager_Redux/static/js/runtime~main.e99378a0.js"
+  }
+]);
